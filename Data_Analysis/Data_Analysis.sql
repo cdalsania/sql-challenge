95186d61ab43e5672f19c59892c9279f31163344
Crystal Dalsania <dalsania.crystal@gmail.com>
2020-11-04 21:39:19 -0500

    Analyzed query 6

--- Data_Analysis/Data_Analysis.sql
+++ Data_Analysis/Data_Analysis.sql
@@ -40,6 +40,15 @@ WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
