28d157fb314f24dc31c3ea35be9cedc47a4f49a9
Crystal Dalsania <dalsania.crystal@gmail.com>
2020-11-04 21:29:19 -0500

    Analyzied query 5

--- Data_Analysis/Data_Analysis.sql
+++ Data_Analysis/Data_Analysis.sql
@@ -33,6 +33,12 @@ JOIN departments
 ON dept_emp.dept_no = departments.dept_no;
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT first_name, last_name,sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
